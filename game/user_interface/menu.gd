25738ffa02c69e6caea684f0b9e00ba77857adff
EGOPOS <egopos@yahoo.com>
2024-01-06 23:03:37 +0300

    another merge

--- game/user_interface/menu.gd
+++ game/user_interface/menu.gd
@@ -5,11 +5,10 @@ extends Control
 
 @onready var settings = $settings
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	play_button.pressed.connect(func(): get_tree().change_scene_to_file("res://game/levels/test_scene.tscn"))
 	settings_button.pressed.connect(func(): settings.visible = !settings.visible)
 	exit_button.pressed.connect(get_tree().quit)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	pass
