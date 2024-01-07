45d6c45157b57531d26a412b6d0776c95ea4c3d0
EGOPOS <egopos@yahoo.com>
2024-01-07 15:55:23 +0300

    player animations

--- game/player/player.gd
+++ game/player/player.gd
@@ -11,8 +11,11 @@ class_name Player
 
 @export var gravity = 1.0
 
-@onready var item_marker_pivot = $Marker3D
 @onready var item_marker = $Marker3D/Marker3D2
+@onready var animation_player = $model/AnimationPlayer
+var animation = "Pidle"
+
+@onready var model = $model
 
 @onready var items_area = $Area3D
 var current_item: Item = null
@@ -21,7 +24,8 @@ func _process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor() and not current_item:
+		animation_player.play("jump")
 		velocity.y = jump_power
 
 	var side_direction = Input.get_axis("movement_left", "movement_right")
@@ -34,16 +38,42 @@ func _process(delta):
 	
 	if current_item:
 		current_item.global_position = lerp(current_item.global_position, item_marker.global_position, delta * 30)
-		
+	
+	model.rotation_degrees.y = lerp(model.rotation_degrees.y, 90.0 * sign(round(velocity.x)) if sign(round(velocity.x)) != 0 else model.rotation_degrees.y, delta * 16.0)
+	
+	update_animation()
+	if animation_player.current_animation != "DropItem" and animation_player.current_animation != "UpItem" and animation_player.current_animation != "jump" or animation_player.current_animation == "run" or animation_player.current_animation == "UpItem_run":
+		animation_player.play(animation)
+
+func update_animation():
+	if round(velocity.y) == 0:
+		if round(abs(velocity.x)) > 0:
+			animation = "UpItem_run" if current_item != null else "run"
+		elif round(abs(velocity.x)) == 0:
+			animation = "UpItem_idle" if current_item != null else "Pidle"
+	elif velocity.y < 0:
+		animation = "jumpDown"
+
 
 func grab_item(item: Item):
 	if current_item:
-		current_item.freeze = false
+		await throw_item()
+		
+	animation_player.play("UpItem")
+	await animation_player.animation_finished
+	
 	current_item = item
 	current_item.freeze = true
 
 func throw_item():
+	if not current_item:
+		return
+	animation_player.play("DropItem")
+	await animation_player.animation_finished
 	current_item.freeze = false
+	current_item.reparent(get_tree().current_scene)
+	current_item.apply_central_impulse(Vector3(velocity.normalized().x, 1, 0) * 10.0)
+	current_item = null
 
 func _input(event):
 	if Input.is_action_just_pressed("interact"):
@@ -51,3 +81,5 @@ func _input(event):
 			if body is Item:
 				grab_item(body)
 				return
+		if current_item:
+			throw_item()
