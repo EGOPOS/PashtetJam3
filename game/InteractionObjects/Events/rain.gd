07f0cf1d194d466a1b6b5527baee85cde1c30698
M3Z1M <nizilplay2004@gmail.com>
2024-01-07 19:19:57 +0300

    Added WorldManager with event switching(need to try it), and added driver event

---
+++ game/InteractionObjects/Events/rain.gd
@@ -0,0 +1,4 @@
+extends Event
+
+func _make_effect(player: Player, hotel_monster: Monster, env: WorldEnvironment) -> void:
+	super(player, hotel_monster, env)
--- game/Monster/monster.gd
+++ game/Monster/monster.gd
@@ -3,14 +3,21 @@ extends Node3D
 
 @export var max_hungry_level: float
 @export var famine: float
+@export var famine_tic: float
 
 @onready var state_machine: MonsterStateMachine = $StateMachine
 @onready var wall_area: Area3D = $Area
+@onready var famine_timer: Timer = $FamineTimer
 
 var velocity: Vector3
 var hungry_level: float
 
 func _ready():
+	hungry_level = max_hungry_level
+	famine_timer.timeout.connect(make_famine)
+	famine_timer.start(famine_tic)
+	Enviroment.monster = self
+
 	wall_area.body_entered.connect(stop_moving)
 	wall_area.body_exited.connect(continue_moving)
 	state_machine.change_state("Moving")
@@ -23,5 +30,6 @@ func continue_moving(body) -> void:
 	if body is WallObject:
 		state_machine.change_state("Moving")
 
-func _process(delta):
-	pass
+func make_famine() -> void:
+	hungry_level -= famine
+	famine_timer.start(famine_tic)
